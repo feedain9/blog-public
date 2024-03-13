f9d9806f4bb8ca9bd4f846c84352e0ff69fb6c9a
feedain <armeniagaming@outlook.fr>
2024-03-13 14:03:37 +0100

    Mise à jour de la clé secrète dans les paramètres Django

--- News/settings.py
+++ News/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i!e@^3!n_4rdv_g*8iz084oyh6m!)1sc9xx+c3mvf(x=2&b&8j'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
